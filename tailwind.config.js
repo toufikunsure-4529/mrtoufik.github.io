711f63cc5aee331763ed9f86f12815c10b5a0bd2
toufikunsure-4529 <toufiksk.ab3@gmail.com>
2025-01-12 18:53:47 +0530

    Added Custom Scroll Bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -34,5 +34,6 @@ export default {
     require("@tailwindcss/forms"),
     require("@tailwindcss/typography"),
     require("@tailwindcss/aspect-ratio"),
+    require("tailwind-scrollbar"),
   ],
 };
